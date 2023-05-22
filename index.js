85acb1371b09c2e987e302885ff9fb81d63f471f
marcos vinicius <marcos.vinicius7170@gmail.com>
2023-05-22 17:16:56 -0300

    seventh

--- index.js
+++ index.js
@@ -2,14 +2,10 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
-const { Router } = require('express')
-const route = new Router()
 
 app.get("/", (req, res) => {
     return res.json("hello world")
 })
 
-const port = process.env.PORT || 5050
+const port = process.env.PORT ?? 5050
 app.listen(port, () => console.log(`Funcionando na porta ${port}`))
-
-module.exports = route

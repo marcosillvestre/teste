5e8e6c78658db51c0626499918e4d7bef540e7db
marcos vinicius <marcos.vinicius7170@gmail.com>
2023-05-24 12:43:44 -0300

    feat new route

--- index.js
+++ index.js
@@ -7,5 +7,10 @@ app.get("/", (req, res) => {
     return res.json("hello world")
 })
 
+app.get("/abcd", (req, res) => {
+
+    return res.json("é so la mesmo")
+})
+
 const port = process.env.PORT ?? 5050
 app.listen(port, () => console.log(`Funcionando na porta ${port}`))
